a1b494bb2f4774cfd645a34aa417fd206b350dfc
indahwii <widowati.indah07@gmail.com>
2020-07-10 16:00:32 +0700

    update html

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+    .wrapper
+      div(class="row columns is-multiline")
+        .card(class="column is-6")
+          .card-images
+            h1
+            img(src="/images/profile.jpg")
+        .card(class="column is-6")
+          .card-content
+            h1.title.is-1.has-text-weight-bold About Me
+            h3.title.is-3 My name is Indah Widowati
+            p.is-size-4 Results-oriented Front-end Developer dedicated to creating and optimizing interactive and user-friendly.Leverage analytical skills and strong attention to detail in order to deliver original andefficient web solutions, provide technical knowledge and expertise, build new websites from start to finish, and successfully manage a team of other software professionals.
+
+          
+          
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+  .wrapper {
+    padding: 30px;
+  }
+
+
+</style>
