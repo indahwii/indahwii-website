7dffe503c38b852ec5370e0944563b198f982ede
indahwii <widowati.indah07@gmail.com>
2020-07-14 15:40:42 +0700

    update style

--- pages/index.vue
+++ pages/index.vue
@@ -11,12 +11,12 @@
             h3.title.is-3 My name is Indah Widowati
             p.is-size-5 Results-oriented Front-end Developer dedicated to creating and optimizing interactive and user-friendly.Leverage analytical skills and strong attention to detail in order to deliver original andefficient web solutions, provide technical knowledge and expertise, build new websites from start to finish, and successfully manage a team of other software professionals.
             .contact
-              h5.is-size-5.has-text-weight-semibold Contact
+              h5.is-size-5.has-text-weight-semibold Contact :
               | widowati.indah07@gmail.com
-            .social-media
-              a.title.is-3(href="https://github.com/indahwii") Github
-              a.title.is-3(href="https://www.linkedin.com/in/indahwii/") LinkedIn
-              a.title.is-3(href="https://www.instagram.com/indahwii/") Instagram
+            .social-media(class="row columns is-multiline")
+              a.title(class="column is-3" href="https://github.com/indahwii") Github
+              a.title(class="column is-3" href="https://www.linkedin.com/in/indahwii/") LinkedIn
+              a.title(class="column is-3" href="https://www.instagram.com/indahwii/") Instagram
           
           
 </template>
@@ -31,7 +31,11 @@ export default {}
   }
 
   .wrapper {
-    padding: 30px;
+    /* padding: 30px; */
+  }
+
+  .card {
+    box-shadow: none;
   }
 
   .contact {
@@ -40,14 +44,12 @@ export default {}
 
   .social-media {
     margin-top: 1rem;
-    display: flex;
-    left: 0;
-    position: relative;
   }
 
-  a{
+
+  a.title{
     color: #000;
-    margin-right: 10px;
+    margin-top: -15px;
   }
 
 
